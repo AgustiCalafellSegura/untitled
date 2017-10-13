1ff65e2e666a552efa4ed9bd63389f619014f572
David Romaní <david@flux.cat>
2017-10-13 10:17:06 +0200

    remove unused todo.txt file

--- src/TestCommand.php
+++ src/TestCommand.php
@@ -6,23 +6,31 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Class TestCommand
+ */
 class TestCommand extends Command
 {
+    /**
+     * Command initialization
+     */
     protected function configure()
     {
         $this
-            // the name of the command (the part after "app/console")
             ->setName('app:test-command')
-
-            // the short description shown while running "php app/console list"
             ->setDescription('Test command.')
-
-            // the full command description shown when running the command with
-            // the "--help" option
-            ->setHelp('This command is only for testing pourposes.')
+            ->setHelp('This command is only for testing purposes.')
         ;
     }
 
+    /**
+     * Command execution
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
        $output->writeln('Hello World!');
